b20d991b3b2928ff21a13ada1bd399a9f2f9640e
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2021-08-25 14:45:13 +0500

    worked on work orders

--- controllers/WorkOrdersController.js
+++ controllers/WorkOrdersController.js
@@ -5,51 +5,23 @@ const WorkOrder = require('../models/WorkOrder');
 
 exports.getWorkOrders = async (req, res, next) => {
     const { _raw, _json, ...userProfile } = req.user;
-    const workOrders = await WorkOrder.find({}, { __v: 0 });
+
+    const [workOrders, users] = await Promise.all([
+        WorkOrder.find({}, { __v: 0 }),
+        User.find({}, { __v: 0 }),
+    ]);
 
     res.render('work_orders', {
         name: userProfile.nickname,
         workOrders,
+        users,
     });
 };
 
-exports.editOngoingProject = async (req, res, next) => {
-    const { _raw, _json, ...userProfile } = req.user;
-    try {
-        const { orderId } = req.params;
-        const order = await Order.findById(orderId).lean();
-        const users = await User.find({});
-        const deliveries = await DeliveryPlace.find({ orderId });
-
-        // const delivery = await DeliveryPlace.findById(
-        //     orderId
-        // ).populate('orderId');
-        // const order = await Order.findById(delivery.orderId._id);
-
-        // if (!delivery) return res.redirect('/');
-
-        const username = await User.findOne({
-            email: req.user.emails[0].value,
-        });
-
-        // let ticket$ = await DevTicket.findOne({ User: username._id });
-
-        // if (!ticket$) {
-        //     ticket$ = { _id: 'dummy' };
-        // }
-
-        res.render('edit-ongoing-project', {
-            name: userProfile.nickname,
-            order,
-            deliveries,
-            // delivery,
-            users,
-            user: username,
-            // ticket: ticket$,
-            editing: true,
-            user$: req.user,
-        });
-    } catch (error) {
-        console.log(error);
-    }
+exports.postWorkOrder = async (req, res, next) => {
+    await WorkOrder.create({
+        ...req.body,
+        user: req.user.nickname,
+    });
+    res.status(200).send();
 };
--- routes/workOrdersRoute.js
+++ routes/workOrdersRoute.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const { getWorkOrders } = require('../controllers/WorkOrdersController');
+const {
+    getWorkOrders,
+    postWorkOrder,
+} = require('../controllers/WorkOrdersController');
 
 const router = express.Router();
 const { secured } = require('../middlewares/secured');
 
 router.get('/work-orders', secured, getWorkOrders);
+router.post('/work-orders', secured, postWorkOrder);
+
 // router.get('/get-ongoing-project/:orderId', secured, editOngoingProject);
 
 module.exports = router;
